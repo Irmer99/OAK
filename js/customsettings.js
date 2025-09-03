0f8f6cd256b726ab5f5a04e79e0ad6f163e73a75
Patricia <pirmerpatricia99@gmail.com>
2025-09-03 14:39:45 +0300

    Update

--- js/customsettings.js
+++ js/customsettings.js
@@ -12,16 +12,23 @@
 
 
 // Scroll to top when a tab is shown
+      // let triggerTabList = [].slice.call(document.querySelectorAll('#nav-tab a'))
+      // triggerTabList.forEach(function (triggerEl) {
+      //   let tabTrigger = new bootstrap.Tab(triggerEl)
+
+      //   triggerEl.addEventListener('shown.bs.tab', function (event) {
+      //     // Scroll to top of the tab content
+      //     document.getElementById('nav-tabContent').scrollIntoView();
+      //   })
+      // })
+
       let triggerTabList = [].slice.call(document.querySelectorAll('#nav-tab a'))
       triggerTabList.forEach(function (triggerEl) {
-        let tabTrigger = new bootstrap.Tab(triggerEl)
-
         triggerEl.addEventListener('shown.bs.tab', function (event) {
           // Scroll to top of the tab content
-          document.getElementById('nav-tabContent').scrollIntoView();
+          document.getElementById('nav-tabContent').scrollIntoView({behavior: 'smooth'});
         })
       })
-
       
 
       
