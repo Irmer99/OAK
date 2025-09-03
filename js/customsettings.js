b1a6472db417e9c57fa4fce58ba1e5adec8200c8
Patricia <pirmerpatricia99@gmail.com>
2025-09-03 14:31:13 +0300

    Moved all custom JS to a singular file. Not to be tampered with

---
+++ js/customsettings.js
@@ -0,0 +1,30 @@
+// dismiss offcanvas when a tab is clicked (for mobile) on menu.html
+      let offcanvasElement = document.getElementById('offcanvasExample');
+      let offcanvas = bootstrap.Offcanvas.getOrCreateInstance(offcanvasElement);
+      let offcanvasTabList = document.querySelectorAll('#offcanvasExample .nav-link');
+
+      offcanvasTabList.forEach(function (triggerEl) {
+        triggerEl.addEventListener('click', function (event) {
+          offcanvas.hide();
+        });
+      });
+
+
+
+// Scroll to top when a tab is shown
+      let triggerTabList = [].slice.call(document.querySelectorAll('#nav-tab a'))
+      triggerTabList.forEach(function (triggerEl) {
+        let tabTrigger = new bootstrap.Tab(triggerEl)
+
+        triggerEl.addEventListener('shown.bs.tab', function (event) {
+          // Scroll to top of the tab content
+          document.getElementById('nav-tabContent').scrollIntoView();
+        })
+      })
+
+      
+
+      
+       
+
+
